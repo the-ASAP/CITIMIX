6c257e5e20c5600db98cb88f291575c515d4cc8b
Makarenko Sergey <Rsmon1982@yandex.ru>
2021-12-14 19:08:07 +0300

    Add header, add customer section

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,10 @@
 import * as $ from 'jquery';
 import '../scss/index.scss';
 import header from '../components/header.html';
-
+import customer from '../components/customer.html';
+import data from '../components/data.html';
 $(() => {
+  $('#root').prepend(data);
+  $('#root').prepend(customer);
   $('#root').prepend(header);
 });
