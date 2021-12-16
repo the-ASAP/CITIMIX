7aa980acbde326b3d2e6108ee234fa4562d1cfec
Makarenko Sergey <Rsmon1982@yandex.ru>
2021-12-16 18:19:11 +0300

    Finish 1 page. Fix bugs

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,10 @@ import * as $ from 'jquery';
 import '../scss/index.scss';
 import header from '../components/header.html';
 import customer from '../components/customer.html';
-import data from '../components/data.html';
+import footer from '../components/footer.html';
 $(() => {
-  $('#root').prepend(data);
-  $('#root').prepend(customer);
-  $('#root').prepend(header);
+  // $('#root').prepend(footer);
+  // $('#root').prepend(customer);
+  // $('#root').prepend(header);
 });
+
